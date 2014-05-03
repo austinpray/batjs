9e648fc82e49c5c9059b0bc7754b14b56ce72210
Austin Pray <austin@austinpray.com>
2014-05-02 19:40:53 -0500

    Initial commit

---
+++ batjs.js
@@ -0,0 +1,35 @@
+/*! batjs v0.0.0 - MIT license */
+
+;(function (global) { function moduleDefinition(/*dependency*/) {
+
+// ---------------------------------------------------------------------------
+
+'use strict';
+
+/**
+ * @param {}
+ * @return {}
+ * @api public
+ */
+
+function batjs() {
+}
+
+/**
+ * Expose batjs
+ */
+
+return batjs;
+
+// ---------------------------------------------------------------------------
+
+} if (typeof exports === 'object') {
+    // node export
+    module.exports = moduleDefinition(/*require('dependency')*/);
+} else if (typeof define === 'function' && define.amd) {
+    // amd anonymous module registration
+    define([/*'dependency'*/], moduleDefinition);
+} else {
+    // browser global
+    global.batjs = moduleDefinition(/*global.dependency*/);
+}}(this));
---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+
+describe('batjs()', function () {
+    it('does something', function () {
+        expect(false).to.equal(false);
+    });
+});
