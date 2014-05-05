9e0d95105f265a925270d401f30740765fa25020
Austin Pray <austin@austinpray.com>
2014-05-04 21:40:22 -0500

    async bats

--- batjs.js
+++ batjs.js
@@ -1,6 +1,6 @@
 /*! batjs v0.0.0 - MIT license */
 
-;(function (global) { function moduleDefinition($) {
+;(function (global) { function moduleDefinition($, async, _) {
 
 // ---------------------------------------------------------------------------
 
@@ -13,47 +13,67 @@
  */
 
 function batjs() {
-  var myTimeline = new TimelineMax({ 
-    autoRemoveChildren: true,
-    paused: true, 
-    stagger: 0.5
-  });
-  var bats = [];
-  function batSource(amount) {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var randomY = function () {
-      return Math.floor(Math.random() * (windowHeight - 0) + 0);
-    }
-    var randomX;
-    var elCache;
-    var randomTime;
-    for(var i = 0; i < amount; i++) {
-      bats.push($('<div class="pixel"></div>'));
-    }
-    $.each(bats, function(i, el) {
-      randomX = Math.random() * (windowHeight - 0) + 0;
-      console.log(el.width());
-      $('body').append(el);
-    });
-    $('.pixel').each(function () {
-      elCache = $(this);
-      randomTime = Math.random();
-      TweenMax.set(elCache, { x: -elCache.width(), y: randomY() });
-      myTimeline.insert(TweenMax.to (elCache, 3, {
-        left: windowWidth + elCache.width(),
-        top: randomY(),
-        ease: randomY() % 2 ? 'easeInSine' : 'easeOutSine',
-        onComplete: function () {
-          $(this.target[0]).remove();
-        }
-      }), randomTime);
-    });
-    myTimeline.play();
+  var context = document.getElementById("batContext");
+  var batCache = [];
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  var randomY = function () {
+    return Math.floor(Math.random() * (windowHeight - 0) + 0);
   }
-  return {
-    batSource: batSource
+  var batQ = async.queue(function (bat, callback) {
+    async.series([
+      function(callback){
+        callback(null, bat.create());
+      },
+      function(callback){
+        callback(null, bat.draw());
+      }
+    ],
+    function () {
+      if(_.isFunction(callback)) {
+        callback();
+      }
+    });
+  }, 100);
+  var Bat = function () {
+    var batEl;
+    return {
+      create: function () {
+        batEl = document.createElement("div");
+        batEl.className = "pixel";
+        context.appendChild(batEl);
+      },
+      draw: function () {
+        TweenMax.to (batEl, 3, {
+          delay: Math.random(),
+          x: windowWidth + batEl.offsetWidth,
+          y: randomY(),
+          ease: randomY() % 2 ? 'easeInSine' : 'easeOutSine',
+          onComplete: function (bat) {
+            bat.destroy();
+          },
+          onCompleteParams: [this]
+        });
+      },
+      destroy: function () {
+        context.removeChild(batEl);
+      }
+    };
   };
+
+  var addBats = function (num) {
+    if(num < 1) {
+      return;
+    } else {
+      batQ.push(new Bat());
+      addBats(num - 1);
+    }
+  };
+  
+  return {
+    addBats: addBats
+  };
+
 }
 
 /**
@@ -72,5 +92,5 @@ return batjs;
     define([/*'dependency'*/], moduleDefinition);
 } else {
     // browser global
-    global.batjs = moduleDefinition(jQuery);
+    global.batjs = moduleDefinition(jQuery, async, _);
 }}(this));
